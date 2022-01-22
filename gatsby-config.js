27b08ba875a5b61c29044316a17e923e0bf7265a
Gramma loreto <smartgramma@hotmail.com>
2022-01-22 18:16:54 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gramma loreto Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Gramma loreto`,
+      summary: `que le gusta investigar y aprender sobre Data-Science, Tech & Environment!!.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
