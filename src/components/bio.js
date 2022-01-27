645ffc8bcf880b62ca6fa4feaec302c43a3d7fd1
Gramma loreto <smartgramma@hotmail.com>
2022-01-26 22:24:11 -0500

    loreto II

--- src/components/bio.js
+++ src/components/bio.js
@@ -28,7 +28,7 @@ const Bio = () => {
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
+  
 
   return (
     <div className="bio">
@@ -46,7 +46,7 @@ const Bio = () => {
         <p>
           Todo el contenido es escrito por <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
+          <a href={`https://grammaloreto.netlify.app/`}>
             También puedes ver mi Portafolio!
           </a>
         </p>
